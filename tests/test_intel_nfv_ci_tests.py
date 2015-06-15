ef616cecbeac1b6a2e8bab45abce3b63fda8d81a
Waldemar Znoinski <waldemar.znoinski@intel.com>
2015-06-15 18:11:56 +0000

    Repository for Intel's tests for NFV CI system
    Repository initialisation
    
    Change-Id: If5a22fbff6627cc2e9dc7c7ccec04157147e81b6

---
+++ tests/test_intel_nfv_ci_tests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_intel-nfv-ci-tests
+----------------------------------
+
+Tests for `intel-nfv-ci-tests` module.
+"""
+
+import unittest
+
+from intel_nfv_ci_tests import intel_nfv_ci_tests
+
+
+class TestIntel_nfv_ci_tests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
