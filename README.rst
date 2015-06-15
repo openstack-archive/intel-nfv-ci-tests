137a633ba2c148772b8a96f75889bafaef8afbe1
Waldemar Znoinski <waldemar.znoinski@intel.com>
2015-06-15 18:11:56 +0000

    Repository for Intel's tests for NFV CI system
    Repository initialisation
    
    Change-Id: If5a22fbff6627cc2e9dc7c7ccec04157147e81b6

---
+++ README.rst
@@ -0,0 +1,20 @@
+===============================
+intel-nfv-ci-tests
+===============================
+
+.. image:: https://img.shields.io/travis/stackforge/intel-nfv-ci-tests.svg
+        :target: https://travis-ci.org/stackforge/intel-nfv-ci-tests
+
+.. image:: https://img.shields.io/pypi/v/intel-nfv-ci-tests.svg
+        :target: https://pypi.python.org/pypi/intel-nfv-ci-tests
+
+
+Repository containing tests for Intel NFV 3rd party CI
+
+* Free software: Apache 2.0 license
+* Documentation: https://intel-nfv-ci-tests.readthedocs.org.
+
+Features
+--------
+
+* TODO
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO(requirements): put package requirements here
+]
+
+test_requirements = [
+    # TODO(test-requirements): put package test requirements here
+]
+
+setup(
+    name='intel_nfv_ci_tests',
+    version='0.1.0',
+    description="Repository containing tests for Intel NFV 3rd party CI",
+    long_description=readme + '\n\n' + history,
+    author="Waldemar Znoinski",
+    author_email='waldemar.znoinski@intel.com',
+    url='https://github.com/stackforge/intel-nfv-ci-tests',
+    packages=[
+        'intel_nfv_ci_tests',
+    ],
+    package_dir={'intel_nfv_ci_tests':
+                 'intel_nfv_ci_tests'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='intel_nfv_ci_tests',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
