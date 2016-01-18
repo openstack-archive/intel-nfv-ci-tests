ddf434d711b970b23eb902364a1243beb03ee866
Stephen Finucane <stephen.finucane@intel.com>
2016-01-18 07:01:26 +0000

    Convert to pbr
    Use pbr for packaging instead of manual setup tools. This allows us to
    take advantage of a number of features provided by pbr, such as
    automated generation of AUTHORS, HISTORY and MANIFEST files.
    
    Note that tests will not work with this version due to missing entry
    points. These will be fixed in the subsequent commit.
    
    Change-Id: I098979a6033afcff9abfbf60de63825f073dc15b

--- setup.py
+++ setup.py
@@ -1,53 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+import setuptools
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
-
-requirements = [
-    # TODO(requirements): put package requirements here
-]
-
-test_requirements = [
-    # TODO(test-requirements): put package test requirements here
-]
-
-setup(
-    name='intel_nfv_ci_tests',
-    version='0.1.0',
-    description="Repository containing tests for Intel NFV 3rd party CI",
-    long_description=readme + '\n\n' + history,
-    author="Waldemar Znoinski",
-    author_email='waldemar.znoinski@intel.com',
-    url='https://github.com/openstack/intel-nfv-ci-tests',
-    packages=[
-        'intel_nfv_ci_tests',
-    ],
-    package_dir={'intel_nfv_ci_tests':
-                 'intel_nfv_ci_tests'},
-    include_package_data=True,
-    install_requires=requirements,
-    license="Apache",
-    zip_safe=False,
-    keywords='intel_nfv_ci_tests',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
+setuptools.setup(
+    setup_requires=['pbr>=1.8'],
+    pbr=True)
